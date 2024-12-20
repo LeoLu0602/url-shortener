25ba08946b43166a8b75be109a5726b4326c3ede
Leo Lu <lyuholu@gmail.com>
2024-12-20 00:51:44 -0500

    feat: define AnalyticsType

--- client/types.tsx
+++ client/types.tsx
@@ -2,3 +2,11 @@ export interface FormType {
     longUrl: string;
     alias: string;
 }
+
+export interface AnalyticsType {
+    alias: string;
+    fullUrl: string;
+    createdAt: string;
+    count: number;
+    lastTimeAccessed: string;
+}
