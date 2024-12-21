98e08ef407f1660452040fbca9777a8ce3e40ecb
Leo Lu <lyuholu@gmail.com>
2024-12-20 19:19:22 -0500

    feat: add boilerplate code for User

--- server/src/main/java/com/example/app/user/UserController.java
+++ server/src/main/java/com/example/app/user/UserController.java
@@ -1,5 +1,23 @@
 package com.example.app.user;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@CrossOrigin(origins = "http://localhost:3000")
 public class UserController {
+
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
     
+    @PostMapping("/api/v1/user/sign-up")
+    public void signUp(@RequestBody User req) {
+        userService.signUp(req);
+    }    
 }
--- server/src/main/java/com/example/app/user/UserService.java
+++ server/src/main/java/com/example/app/user/UserService.java
@@ -1,5 +1,17 @@
 package com.example.app.user;
 
+import org.springframework.stereotype.Service;
+
+@Service    
 public class UserService {
     
+    private final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public void signUp(User req) {
+        
+    }
 }
