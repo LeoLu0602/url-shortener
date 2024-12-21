9156bbf38dd023ad0da9d2da9d8b7ee0768968d8
Leo Lu <lyuholu@gmail.com>
2024-12-20 22:20:03 -0500

    feat: finish sign-up endpoint

--- server/src/main/java/com/example/app/user/UserService.java
+++ server/src/main/java/com/example/app/user/UserService.java
@@ -1,6 +1,13 @@
 package com.example.app.user;
 
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+import java.util.Base64;
+
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 @Service    
 public class UserService {
@@ -12,6 +19,41 @@ public class UserService {
     }
 
     public void signUp(User req) {
-        
+        String email = req.getEmail();
+        String name = req.getName();
+        String password = req.getPassword();
+
+        if (userRepository.findByEmail(email).isEmpty()) {
+            String hashedPassword = hashPassword(password);
+
+            if (hashedPassword.isEmpty()) {
+                throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "The server encountered an internal error.");
+            }
+
+            userRepository.save(new User(email, name, this.hashPassword(hashedPassword)));
+        } else {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Account already exists.");
+        }
+    }
+
+    public String hashPassword(String password) {
+        try {
+            String salt = generateSalt(password);
+            MessageDigest md = MessageDigest.getInstance("SHA-256");
+            byte[] hashedBytes = md.digest((password + salt).getBytes());
+    
+            return Base64.getEncoder().encodeToString(hashedBytes);
+        } catch (NoSuchAlgorithmException e) {
+            return "";
+        }
+    }
+
+    public static String generateSalt(String password) {
+        SecureRandom secureRandom = new SecureRandom();
+        byte[] salt = new byte[16];
+
+        secureRandom.nextBytes(salt);
+
+        return Base64.getEncoder().encodeToString(salt);
     }
 }
