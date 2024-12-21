6f4bbc063a2d30932634c2f0601c502e1cd2ea40
Leo Lu <lyuholu@gmail.com>
2024-12-20 22:36:43 -0500

    feat: add SignUpRequest

---
+++ server/src/main/java/com/example/app/user/SignUpRequest.java
@@ -0,0 +1,37 @@
+package com.example.app.user;
+
+public class SignUpRequest {
+    private String email;
+    private String name;
+    private String password;
+
+    public SignUpRequest(String email, String name, String password) {
+        this.email = email;
+        this.name = name;
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return this.email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return this.password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
