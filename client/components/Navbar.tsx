dd71eb193ced7823ba42b959b0765de96ae201d9
Leo Lu <lyuholu@gmail.com>
2025-01-03 19:21:26 -0500

    fix: only show dashboard in navbar if signed in

--- client/components/Navbar.tsx
+++ client/components/Navbar.tsx
@@ -16,23 +16,25 @@ export default function Navbar() {
     return (
         <nav className="fixed top-4 right-12 z-10">
             <ul className="flex bg-[#1f8244] h-[44px] text-white rounded-lg">
-                <li>
-                    <Link
-                        className="h-full flex items-center px-4 hover:bg-[#2bb35d] rounded-lg"
-                        href="/dashboard"
-                    >
-                        Dashboard
-                    </Link>
-                </li>
                 {auth ? (
-                    <li>
-                        <button
-                            className="h-full flex items-center px-4 hover:bg-[#2bb35d] rounded-lg"
-                            onClick={signOut}
-                        >
-                            Sign Out
-                        </button>
-                    </li>
+                    <>
+                        <li>
+                            <Link
+                                className="h-full flex items-center px-4 hover:bg-[#2bb35d] rounded-lg"
+                                href="/dashboard"
+                            >
+                                Dashboard
+                            </Link>
+                        </li>
+                        <li>
+                            <button
+                                className="h-full flex items-center px-4 hover:bg-[#2bb35d] rounded-lg"
+                                onClick={signOut}
+                            >
+                                Sign Out
+                            </button>
+                        </li>
+                    </>
                 ) : (
                     <>
                         <li>
