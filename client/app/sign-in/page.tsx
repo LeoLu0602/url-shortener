113acf43df8533dc51945e537e0f48670de0a1b5
Leo Lu <lyuholu@gmail.com>
2025-01-03 19:30:22 -0500

    fix: autocomplete -> autoComplete

--- client/app/sign-in/page.tsx
+++ client/app/sign-in/page.tsx
@@ -70,7 +70,7 @@ export default function SignIn() {
                             value={formData.email}
                             onChange={handleChange}
                             required={true}
-                            autocomplete="off"
+                            autoComplete="off"
                         />
                     </label>
                 </div>
